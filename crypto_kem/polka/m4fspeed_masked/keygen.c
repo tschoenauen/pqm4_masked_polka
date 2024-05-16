42acb163a42f14797364f62a8315a3d04db05cf6
Thibaud Schoenauen <thibaud.schoenauen@student.uclouvain.be>
2024-05-16 10:32:30 +0200

    Removed file lecture in keygen. Using hardcoded default keys instead

--- crypto_kem/polka/m4fspeed_masked/keygen.c
+++ crypto_kem/polka/m4fspeed_masked/keygen.c
@@ -4,28 +4,29 @@
 
 
 int keygen(plk_sk* sk, plk_pk* pk){
-    FILE* key_file = fopen("key_0","r");
+    /*FILE* key_file = fopen("key_0","r");
     if(key_file == NULL){
         printf("ERROR : Unable to open ressource file");
         return 1;   
-    }
+    }*/
 
     //The key file stores data on 16bits, the buffers reads them as such and then we cast the values on c_int.
-    uint32_t buf[N];
-    fread(buf, sizeof(uint32_t), N, key_file);
-    for(int i = 0; i < N; i++) pk->a[i] = (c_int) buf[i];
+    //uint32_t buf[N];
+    //fread(buf, sizeof(uint32_t), N, key_file);
+    for(int i = 0; i < N; i++) pk->a[i] = default_a;
 
-    fread(buf, sizeof(uint32_t), N, key_file);
-    for(int i = 0; i < N; i++) pk->b[i] = (c_int) buf[i];
+    //fread(buf, sizeof(uint32_t), N, key_file);
+    for(int i = 0; i < N; i++) pk->b[i] = default_b;
 
-    fread(buf, sizeof(uint32_t), N, key_file);
-    for(int i = 0; i < N; i++) pk->b_inv[i] = (c_int) buf[i];
+    //fread(buf, sizeof(uint32_t), N, key_file);
+    for(int i = 0; i < N; i++) pk->b_inv[i] = default_b_inv;
 
-    fread(buf, sizeof(uint32_t), N, key_file);
-    for(int i = 0; i < N; i++) sk->s[i] = (c_int) buf[i];
+    //fread(buf, sizeof(uint32_t), N, key_file);
+    for(int i = 0; i < N; i++) sk->s[i] = default_s;
 
     sk->pk = pk;
-    fclose(key_file);
+    //fclose(key_file);
+
 
     return 0;
 }
