dbd70b41a13657c5cd33580018229f8433954c43
Thibaud Schoenauen <tschoenauen@Morgane>
2024-05-21 21:47:03 +0200

    Working polka ENC

--- crypto_kem/polka/m4fspeed_masked/kem.c
+++ crypto_kem/polka/m4fspeed_masked/kem.c
@@ -22,9 +22,11 @@
 #include "randombytes.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 
-#include "debug_dev.h"
+void* memcpy(void* dest, const void* src,size_t n){
+	for(int i = 0; i < (int)n; i ++) ((char*)dest)[i] = ((char*)src)[i];
+	return src;
+}
 
 int sha256_build_key(poly r, poly e1, poly e2, unsigned char* key){
     uint8_t hash_feed[3*N*4];  //Contains coefficients of 3 polynomials containing N coeffs each four times bigger than uint8.
@@ -73,11 +75,11 @@ int convert_ct_vec2str(const unsigned char* src, plk_cipher* rcv){
 }
 
 int convert_ct_str2vec(const plk_cipher* src, unsigned char* rcv){
-  memcpy(&rcv[N*0],   src->c1  , sizeof(poly));
-  memcpy(&rcv[N*1],   src->c2  , sizeof(poly));
-  memcpy(&rcv[N*2],  &(src->c0_l), sizeof(unsigned long long));
-  memcpy(&rcv[N*2+8], src->c0  , sizeof(char)*src->c0_l);
-	
+  memcpy(&rcv[N*0],     src->c1   , sizeof(poly));
+  memcpy(&rcv[N*1],     src->c2   , sizeof(poly));
+  memcpy(&rcv[N*2],   &(src->c0_l), sizeof(unsigned long long));
+  memcpy(&rcv[(N*2)+8], src->c0   , sizeof(char)*src->c0_l);
+
   return 0;
 }
 
@@ -96,13 +98,13 @@ int convert_ct_str2vec(const plk_cipher* src, unsigned char* rcv){
  * Returns 0 (success)
  **************************************************/
 int crypto_kem_keypair(unsigned char *pk, unsigned char *sk) {
-  DEBUG_PRINT("Generating keys...");
+  ////DEBUG_PRINT("Generating keys...");
   plk_pk public_key;
   plk_sk secret_key;
-  keygen(&secret_key,&public_key);
+  keygen(&secret_key,&public_key); //Links the keys. 
   convert_pk_str2vec(&public_key,pk);
   convert_sk_str2vec(&secret_key,sk);
-  DEBUG_PRINT("SUCCESS");
+  ////DEBUG_PRINT("SUCCESS");
   return 0;
 }
 
@@ -123,14 +125,16 @@ int crypto_kem_keypair(unsigned char *pk, unsigned char *sk) {
  **************************************************/
 int crypto_kem_enc(unsigned char *ct, unsigned char *ss,
                    const unsigned char *pk) {
-  DEBUG_PRINT("Encrypting...");
-//  unsigned char npub[16] = "000000000000000"; //Does not impact performances so it is a fix value for performances benchmarks.
-//  plk_pk public_key;
-//  convert_pk_vec2str(pk,&public_key);
-//  plk_cipher cipher_text;	
-//  polka_encrypt(ss,CRYPTO_BYTES,&public_key, &cipher_text, npub, sha256_build_key, saturnin_aead_encrypt);
-//  convert_ct_str2vec(&cipher_text,ct);
-  DEBUG_PRINT("SUCCESS");
+  //DEBUG_PRINT("Encrypting...");
+  unsigned char npub[16] = "000000000000000"; //Does not impact performances so it is a fix value for performances benchmarks.
+  plk_pk public_key;
+  convert_pk_vec2str(pk,&public_key);
+  unsigned char c0[1024];
+  plk_cipher cipher_text;
+  cipher_text.c0 = c0;
+  polka_encrypt(ss,CRYPTO_BYTES,&public_key, &cipher_text, npub, sha256_build_key, saturnin_aead_encrypt);
+  convert_ct_str2vec(&cipher_text,ct);
+  //DEBUG_PRINT("SUCCESS");
   return 0;
 }
 
@@ -153,14 +157,18 @@ int crypto_kem_enc(unsigned char *ct, unsigned char *ss,
  **************************************************/
 int crypto_kem_dec(unsigned char *ss, const unsigned char *ct,
                    const unsigned char *sk) {
-  DEBUG_PRINT("Decrypting...");
-//  unsigned char npub[16] = "000000000000000"; //Does not impact performances so it is a fix value for performances benchmarks.
-//  plk_sk secret_key;
-//  convert_sk_vec2str(sk,&secret_key);
-//  plk_cipher cipher_text;
-//  convert_ct_vec2str(ct,&cipher_text);
-//  unsigned long long ss_l;
-//  polka_decrypt(&cipher_text, &secret_key, ss, &ss_l, npub, sha256_build_key, saturnin_aead_decrypt);
-  DEBUG_PRINT("SUCCESS");
+  //DEBUG_PRINT("Decrypting...");
+  unsigned char npub[16] = "000000000000000"; //Does not impact performances so it is a fix value for performances benchmarks.
+  plk_sk secret_key;
+  plk_pk public_key;
+  secret_key.pk = &public_key;
+  convert_sk_vec2str(sk,&secret_key);
+  unsigned char c0[1024];
+  plk_cipher cipher_text;
+  cipher_text.c0 = c0;
+  convert_ct_vec2str(ct,&cipher_text);
+  unsigned long long ss_l;
+  //polka_decrypt(&cipher_text, &secret_key, ss, &ss_l, npub, sha256_build_key, saturnin_aead_decrypt);
+  //DEBUG_PRINT("SUCCESS");
   return 0;
 }
--- crypto_kem/polka/m4fspeed_masked/params.h
+++ crypto_kem/polka/m4fspeed_masked/params.h
@@ -4,7 +4,7 @@
 
 #define POLKA_SECRETKEYBYTES 16384 // Size of the secret key.
 #define POLKA_PUBLICKEYBYTES 12288 // Size of the public key.
-#define POLKA_CIPHERTEXTBYTES 16   // Size of the message/
+#define POLKA_CIPHERTEXTBYTES 16   // Size of the message.
 #define POLKA_SSBYTES 8248         // Size of the ciphertext.
 
 #define N 1024      // Polynomial degree. Standard key element
